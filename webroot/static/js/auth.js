0ca4615ae1afdf65cdca9241d213f584b1dceb90
jzide <jzide@JzWork>
2024-07-16 19:44:47 +0900

    update

---
+++ webroot/static/js/auth.js
@@ -0,0 +1,50 @@
+const Auth = {};
+
+Auth._endpoint = new Keycloak({
+	url: 'https://eqpls.com/auth',
+	realm: 'eqpls',
+	clientId: 'openid'
+});
+
+Auth.init =(resultHandler, errorHandler)=> {
+	try {
+		Auth._endpoint.onAuthSuccess =()=> {
+			Auth.getTokens((tokens)=>{
+				let authHeader = `Bearer ${tokens.accessToken}`;
+				Auth.RestHeaders = {
+					"Content-Type": "application/json; charset=utf-8",
+					"Accept": "application/json; charset=utf-8",
+					"Authorization": authHeader,
+					"Realm": "admin"
+				}
+				if (resultHandler) { resultHandler(Auth); }
+			});
+		};
+		Auth._endpoint.onAuthError =()=> {
+			console.error('failed to initialize keycloak:', e);
+			if (errorHandler) { errorHandler(e); }
+		};
+		Auth.authentication = Auth._endpoint.init({
+			onLoad: 'login-required'
+		});
+	} catch (e) {
+		console.error('failed to initialize keycloak:', e);
+		if (errorHandler) { errorHandler(e); }
+	}
+};
+
+Auth.getTokens =(resultHandler, errorHandler)=> {
+	if (Auth.authentication) {
+		let tokens = {
+			realm: Auth._endpoint.realm,
+			accessToken: Auth._endpoint.token,
+			refreshToken: Auth._endpoint.refreshToken
+		};
+		if (resultHandler) { resultHandler(tokens); }
+		return tokens;
+	} else {
+		console.error('could not find any auth information');
+		if (errorHandler) { errorHandler(e); }
+		return null;
+	}
+};
--- webroot/static/js/eqpls.js
+++ webroot/static/js/eqpls.js
@@ -1,1 +1,13 @@
 // javascript here
+
+Auth.init((auth)=> {
+	console.log(auth.RestHeaders);
+	fetch("/secret/test/case1", {
+		headers: auth.RestHeaders
+	}).then((res)=> {
+		if (!res.ok) { throw res; }
+		return res.text()
+	}).then((data)=> {
+		console.log(data);
+	});
+});
